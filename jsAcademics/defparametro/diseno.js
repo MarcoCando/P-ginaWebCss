41bf31d009500096dc1ade65e14d60a2a2e876c9
Andy Cando <andy.cando.20@gmail.com>
2024-08-30 12:28:31 -0500

    JavaScript

--- jsAcademics/defparametro/diseno.js
+++ jsAcademics/defparametro/diseno.js
@@ -61,43 +61,46 @@ function renderTable() {
                 const number = extractNumber(parts[0]);
                 const description = extractDescription(parts[0]);
                 const credits = extractCredits(parts[1]);
+                const semestre = extractSemestre(parts[1]);
                 if (!isNaN(parseInt(credits))) {
                     totalCredits += parseInt(credits);
                 }
                 return `
-        <tr>
-          <td style="text-align: center;"><input type="text" value="${number}" data-index="${index}" class="input-number"></td>
-          <td><input type="text" value="${description}" data-index="${index}" class="input-description"></td>
-          <td style="text-align: center;"><input type="number" value="${credits}" data-index="${index}" class="input-credits"></td>
-          <td style="text-align: center;">
-            <button class="btn btn-sm btn-primary" data-index="${index}" data-description="${description}">Crear</button>
-          </td>
-        </tr>
-      `;
+                    <tr>
+                        <td style="text-align: center;"><input type="text" value="${number}" data-index="${index}" class="input-number"></td>
+                        <td><input type="text" value="${description}" data-index="${index}" class="input-description"></td>
+                        <td style="text-align: center;"><input type="number" value="${credits}" data-index="${index}" class="input-credits"></td>
+                        <td style="text-align: center;"><input type="text" value="${semestre}" data-index="${index}" class="input-semestre"></td>
+                        <td style="text-align: center;">
+                            <button class="btn btn-sm btn-primary" data-index="${index}" data-description="${description}">Crear</button>
+                        </td>
+                    </tr>
+                `;
             })
             .filter(Boolean)
             .join("");
 
         const totalRow = `
-    <tr>
-      <td colspan="3" style="text-align: center;">Total Créditos</td>
-      <td style="text-align: center;">${totalCredits}</td>
-    </tr>
-    `;
+            <tr>
+                <td colspan="4" style="text-align: center;">Total Créditos</td>
+                <td style="text-align: center;">${totalCredits}</td>
+            </tr>
+        `;
 
         tableHTML = `
-    <table class="table table-hover table-bordered" style="font-size: 12px;">
-      <thead style="background-color: #cacaca; text-align: center; color: #3d3d3dc9;">
-        <tr>
-          <th>Number</th>
-          <th>Nombre Asignatura</th>
-          <th>Créditos</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>${rows}${totalRow}</tbody>
-    </table>
-    `;
+            <table class="table table-hover table-bordered" style="font-size: 12px;">
+                <thead style="background-color: #cacaca; text-align: center; color: #3d3d3dc9;">
+                    <tr>
+                        <th>Number</th>
+                        <th>Nombre Asignatura</th>
+                        <th>Créditos</th>
+                        <th>Semestre</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>${rows}${totalRow}</tbody>
+            </table>
+        `;
 
         document.getElementById("tableContainer").innerHTML = tableHTML;
     }
@@ -105,7 +108,6 @@ function renderTable() {
     // Volver a enlazar los eventos de los botones
     rebindButtonEvents();
 }
-
 
 function extractNumber(text) {
     const match = text.match(/\{?\d+\}?/);
@@ -120,6 +122,11 @@ function extractDescription(text) {
 function extractCredits(text) {
     const match = text && text.match(/\d+/);
     return match ? match[0] : "";
+}
+
+function extractSemestre(text) {
+    const match = text && text.match(/semestre\s(\d+)/);
+    return match ? match[1] : "";
 }
 
 async function SpinnerDiseño() {
@@ -142,7 +149,6 @@ async function handleButtonClickDiseño() {
     }
 }
 
-// Funciones para manejar los clics en los botones "Generar" y "Ver"
 function handleGenerar(event) {
     event.preventDefault(); // Prevenir comportamiento de recarga
     const index = event.target.getAttribute('data-index');
@@ -160,7 +166,6 @@ function handleGenerar(event) {
     }
 }
 
-
 function handleVer(event) {
     event.preventDefault(); // Prevenir comportamiento de recarga
     const index = event.target.getAttribute('data-index');
@@ -172,12 +177,12 @@ function handleVer(event) {
     }
 }
 
-// Función para actualizar sessionStorage con los valores actuales de los inputs
 function updateSessionStorageWithCurrentValues() {
     // Obtener todos los inputs de la tabla
     const numberInputs = document.querySelectorAll('.input-number');
     const descriptionInputs = document.querySelectorAll('.input-description');
     const creditsInputs = document.querySelectorAll('.input-credits');
+    const semestreInputs = document.querySelectorAll('.input-semestre');
 
     // Asegurarse de que programData sea un array
     programData = programData || { name: [] };
@@ -201,32 +206,24 @@ function updateSessionStorageWithCurrentValues() {
         programData.name[index].credits = input.value;
     });
 
+    semestreInputs.forEach(input => {
+        const index = input.getAttribute('data-index');
+        programData.name[index] = programData.name[index] || {}; // Asegurarse de que el objeto existe
+        programData.name[index].semestre = input.value;
+    });
+
     // Guardar la tabla editada en sessionStorage
     sessionStorage.setItem('programData', JSON.stringify(programData));
 }
 
-// Restaurar el contenido de la tabla desde sessionStorage al cargar la página
-/*window.onload = function () {
-    const savedTableHTML = sessionStorage.getItem('tableContainer');
-    const savedProgramData = sessionStorage.getItem('programData');
-
-    if (savedTableHTML) {
-        document.getElementById('tableContainer').innerHTML = savedTableHTML;
-        if (savedProgramData) {
-            programData = JSON.parse(savedProgramData);
-            // Opcionalmente, puedes re-renderizar la tabla si es necesario
-            // renderTable();
-        }
-        rebindButtonEvents(); // Volver a enlazar los eventos de los botones
-    } else {
-        handleButtonClickDiseño();
-    }
-};*/
+window.onload = function () {
+    renderTable(); // Renderizar la tabla al cargar la página
+};
 
 function rebindButtonEvents() {
     const createButtons = document.querySelectorAll('.btn-primary');
     const viewButtons = document.querySelectorAll('.btn-secondary');
-    const inputFields = document.querySelectorAll('.input-number, .input-description, .input-credits');
+    const inputFields = document.querySelectorAll('.input-number, .input-description, .input-credits, .input-semestre');
 
     createButtons.forEach((button) => {
         button.addEventListener('click', handleGenerar);
@@ -255,13 +252,11 @@ function handleInputChange(event) {
     } else if (fieldType.includes('input-credits')) {
         programData.name[index] = programData.name[index] || {}; // Asegurarse de que el objeto existe
         programData.name[index].credits = value;
+    } else if (fieldType.includes('input-semestre')) {
+        programData.name[index] = programData.name[index] || {}; // Asegurarse de que el objeto existe
+        programData.name[index].semestre = value;
     }
 
-    // Actualizar y guardar la tabla en sessionStorage
-    sessionStorage.setItem('programData', JSON.stringify(programData));
+    // Actualizar los datos en sessionStorage
+    updateSessionStorageWithCurrentValues();
 }
-window.onload = function () {
-    renderTable(); // Renderizar la tabla al cargar la página
-};
-
-/* FIN DE DISEÑO CURRICULAR */
